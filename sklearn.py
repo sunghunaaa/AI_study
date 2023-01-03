eb2861069b3304fb23fd86f9e98422b112ffb004
sunghunaaa <121652427+sunghunaaa@users.noreply.github.com>
2023-01-03 18:09:33 +0900

    Update sklearn.py

--- sklearn.py
+++ sklearn.py
@@ -15,4 +15,4 @@ x_train, x_test, y_train, y_test = train_test_split(
 #train_size, test_size 둘 중 하나만 작성해도 됨.
 #shuffle 작성 x시 True가 기본값임
 #random_state 작성 x시 계속해서 랜덤하게 뽑기 됨. random_state에 난수 입력시 해당 난수는 고정이라 랜덤하게 뽑힌 숫자가 똑같이 계속 나온다.
-           
+#행렬 변수에도 사용가능           
