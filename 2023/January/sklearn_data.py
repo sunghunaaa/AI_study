0f17d060b8d99576db1bc0f9907cce8181c67096
sunghunaaa <121652427+sunghunaaa@users.noreply.github.com>
2023-01-04 22:34:48 +0900

    Create sklearn_data.py

---
+++ 2023/January/sklearn_data.py
@@ -0,0 +1,11 @@
+from sklearn.datasets import load_diabetes
+
+datasets = load_diabetes()
+x = datasets.data
+y = datasets.target
+
+print(datasets.feature_names) 
+print(datasets.DESCR)
+
+print(x.shape) # 열확인하여 dim값 확인한다.
+print(y.shape) # x의 행과 일치하는 확인, output dim 값 확인한다.
