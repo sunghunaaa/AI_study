cf97e34c847da19a56e1d6b9836f3be5e2c44a08
sunghunaaa <121652427+sunghunaaa@users.noreply.github.com>
2023-01-08 19:02:02 +0900

    Update sklearn.py

--- 2023/January/sklearn.py
+++ 2023/January/sklearn.py
@@ -16,3 +16,8 @@ x_train, x_test, y_train, y_test = train_test_split(
 #shuffle 작성 x시 True가 기본값임
 #random_state 작성 x시 계속해서 랜덤하게 뽑기 됨. random_state에 난수 입력시 해당 난수는 고정이라 랜덤하게 뽑힌 숫자가 똑같이 계속 나온다.
 #행렬 변수에도 사용가능           
+
+# *추가
+model.fit(x_train,y_train,epochs=200,batch_size=32
+          ,validation=0.3)
+# validation - train_size의 30% 데이터로 훈련 단계에 검증을 추가함.
