a6df8a31961edc0234e33a861abc2d8755f7bbfe
sunghunaaa <121652427+sunghunaaa@users.noreply.github.com>
2023-01-15 03:19:06 +0900

    Update hist plt.py

--- 2023/January/hist plt.py
+++ 2023/January/hist plt.py
@@ -1,6 +1,6 @@
 import matplotlib.pyplot as plt
 plt.figure(figsize=(9,6)) # 그래프 크기
-plt.plot(hist.history['loss'],c='red',marker='.',label='loss') #c= color
+plt.plot(hist.history['loss'],c='red',marker='.',label='loss') #c= color ,marker= 선무늬
 plt.plot(hist,history['val_loss'],c='blue',marker='.',label='val_loss')
 plt.grid() # 모눈종이형태 그래프
 plt.xlabel('epochs') # x축
