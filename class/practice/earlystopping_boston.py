d1f761b2721bf0044e379db0991b734a4bcc9f50
sunghunaaa <121652427+sunghunaaa@users.noreply.github.com>
2023-01-20 22:39:13 +0900

    Create earlystopping_boston.py

---
+++ class/practice/earlystopping_boston.py
@@ -0,0 +1,64 @@
+from sklearn.datasets import load_boston
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense
+from sklearn.model_selection import train_test_split
+
+#1. datasets
+datasets = load_boston()
+x = datasets.data
+y = datasets.target
+
+print(x.shape, y.shape)  # (506,13)  (506,)
+
+x_train,x_test,y_train,y_test = train_test_split(x,y,shuffle=True,train_size=0.8,random_state=333)
+
+#2. model
+model = Sequential()
+model.add(Dense(6, input_dim=13))  
+model.add(Dense(6, input_shape=(13,)))    
+model.add(Dense(6))
+model.add(Dense(6))
+model.add(Dense(6))
+model.add(Dense(1))
+
+
+#3.compile, fit
+
+model.compile(loss='mse', optimizer='adam')
+
+from tensorflow.keras.callbacks import EarlyStopping
+earlyStopping = EarlyStopping(monitor='val_loss', mode = 'min',patience=20, restore_best_weights=True, verbose = 1)
+
+hist = model.fit(x_train,y_train, epochs=100, batch_size=32
+          ,validation_split= 0.2, 
+          verbose = 1, callbacks=[earlyStopping]
+          )
+
+#4. evaluate,predict
+loss = model.evaluate(x_test, y_test)
+print('loss : ' , loss)
+print("====================================")
+print( hist)  #<tensorflow.python.keras.callbacks.History object at 0x28b74f790>
+print("====================================")
+print(hist.history)
+print("====================================")
+print(hist.history['loss']) # -> history에서 loss 값만 나옴 
+print("====================================")
+print(hist.history['val_loss']) # -> history에서 val_loss 값만 나옴 
+print("====================================")
+
+import matplotlib.pyplot as plt
+plt.figure(figsize=(9,6))
+plt.plot(hist.history['loss'],c = 'red', marker = '.', label = 'loss') #x는 epoch로 순서대로 임, 따라서 안 써줘도 됨
+plt.plot(hist.history['val_loss'], c='blue', marker = '.',label = 'val_loss' )
+plt.grid()  # 격자가 생김(모눈종이형태)
+plt.xlabel('epochs')
+plt.ylabel('loss')
+plt.title('boston loss')
+plt.legend(loc='upper right') # 그래프에 빨간 선이 뭔지, 파란 선이 뭔지 나옴, 위치는 자동으로 나오고 , 직접 지정도 가능하다. loc = local // upper or down , right of left
+plt.show()
+
+
+
+
+
